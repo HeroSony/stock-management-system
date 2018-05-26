41335ba19b861ec89f5286b20f7f4ef4008411d7
root <root@herosony.com>
2018-05-26 14:25:29 +0000

    change configuration

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/SMA_v3.2.12/';
+$config['base_url'] = 'http://stock.consultingcan.com/';
 
 /*
 |--------------------------------------------------------------------------
